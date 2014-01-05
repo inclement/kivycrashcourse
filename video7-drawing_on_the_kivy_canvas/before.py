85449cd29ef058f9cc31e8f53ad575e9d8014f9b
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-01-05 17:50:15 +0000

    Added video7 material

---
+++ video7-drawing_on_the_kivy_canvas/before.py
@@ -0,0 +1,26 @@
+
+from kivy.app import App
+
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+
+from kivy.properties import ListProperty, ObjectProperty
+
+import random
+
+class ScatterTextWidget(BoxLayout):
+    text_colour = ObjectProperty([1, 0, 0, 1])
+    
+    def change_label_colour(self, *args):
+        colour = [random.random() for i in xrange(3)] + [1]
+        self.text_colour = colour
+    
+class TutorialApp(App):
+    def build(self):
+        return ScatterTextWidget()
+
+if __name__ == "__main__":
+    TutorialApp().run()
